bf05ac74294e75f2fb01ad73a48cefbf55cb7920
autumn <AutKevin@users.noreply.github.com>
2019-11-27 17:07:22 +0800

    ssr
    ssr

--- VPN/centOS+Shadowsocks/shadowsocksR.sh
+++ VPN/centOS+Shadowsocks/shadowsocksR.sh
@@ -381,7 +381,7 @@ firewall_set(){
 # Config ShadowsocksR
 config_shadowsocks(){
 		mkdir /etc/shadowsocksR/
-    cat > /etc/shadowsocksR/shadowsocksR.json<<-EOF
+    cat > /etc/shadowsocksR/shadowsocksr-config.json<<-EOF
 {
     "server":"0.0.0.0",
     "server_ipv6":"[::]",
@@ -476,7 +476,7 @@ uninstall_shadowsocksr(){
         elif check_sys packageManager apt; then
             update-rc.d -f shadowsocks remove
         fi
-        rm -f /etc/shadowsocksR/shadowsocksR.json
+        rm -f /etc/shadowsocksR/shadowsocksr-config.json
         rm -f /etc/init.d/shadowsocksR
         rm -f /var/log/shadowsocks.log
         rm -rf /usr/local/shadowsocks
